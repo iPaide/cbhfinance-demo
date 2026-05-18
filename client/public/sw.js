bd653fa873b966f82c3e1bb907993a7de91102b7
Solomon Richardson <ipaide@Solomons-MacBook-Pro.local>
2026-05-18 19:34:09 -0400

    Add branded icons and social preview image

--- client/public/sw.js
+++ client/public/sw.js
@@ -3,7 +3,10 @@ const CACHE_NAME = "cbhfinance-pwa-v1";
 const APP_SHELL = [
   "/",
   "/manifest.webmanifest",
-  "/icons/icon.svg"
+  "/icons/icon.svg",
+  "/icons/icon-192.png",
+  "/icons/icon-512.png",
+  "/og-image.png"
 ];
 
 self.addEventListener("install", (event) => {
